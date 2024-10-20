cb400079aaaa6020bc265eb0c69678bedcc898bd
Priyanshu Patel <priyanshu.patel@codevyasa.com>
2024-10-20 18:37:46 +0530

    Fix app run

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # gym-management-lld
 
+### Steps to run the App
+
+1. Build the app
+
+```sh
+npm run build
+```
+
+2. Run the app
+
+```sh
+npm run start
+```
+
+### Notes
+
 - We will have different slots for Weights and Cardio
 - Each slot can have maximum of 3 bookings
--- User.ts
+++ User.ts
@@ -1,6 +1,15 @@
+type Gender = 'male' | 'female' | 'other';
+
 export class User {
   name: string;
   email: string;
   password: string; // Hashed
-  gender: 'male' | 'female' | 'other';
+  gender: Gender;
+
+  constructor(name: string, email: string, password: string, gender: Gender) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.gender = gender;
+  }
 }
